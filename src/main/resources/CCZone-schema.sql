a113358c20104410b171175118ff9b0811dbce7a
treesacherian <treesa.cherian@gmail.com>
2024-03-06 16:09:55 +0000

    testing done partially

--- src/main/resources/CCZone-schema.sql
+++ src/main/resources/CCZone-schema.sql
@@ -2,11 +2,11 @@ DROP TABLE `item` CASCADE;
 DROP TABLE `cart`CASCADE;
 CREATE TABLE `cart`(
 `id` INT PRIMARY KEY AUTO_INCREMENT,
-`buyer` VARCHAR);
+`buyer` VARCHAR(100));
 
 CREATE TABLE `item`(
 `id` INT PRIMARY KEY AUTO_INCREMENT,
-`item_name` VARCHAR,
+`item_name` VARCHAR(100),
 `item_price` DOUBLE,
 `item_quantity` INT,
 `cart_id` INT,
